0695d351fc15eb1895ac75373950c5c8573eb0c5
urgyan <urgyanue195125ece@gmail.com>
2022-02-04 21:04:08 +0530

    add scss f

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const usersController = require('../controllers/users_controller');
+const usersController = require('../controllers/user_controller');
 
 router.get('/profile', passport.checkAuthentication, usersController.profile);
 
