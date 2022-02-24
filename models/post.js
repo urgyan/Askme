f5e335e1a6bf91ee5764f119e8f1011b71434294
urgyan <urgyanue195125ece@gmail.com>
2022-02-24 20:42:59 +0530

    onmodel add in post .js

--- models/post.js
+++ models/post.js
@@ -1,26 +1,31 @@
 const mongoose = require('mongoose');
 
-const likeSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
     user:{
-        type:mongoose.Schema.Types.ObjectId ,
+        type: mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
-    //this defines the object id of the liked object means on which like is made post or comment
-    likeable:{
-        type:mongoose.Schema.Types.ObjectId ,
-        required:true,
-        refPath:'onModel'
-    },
-    //this field is used to define the type of liked object since it is dynamic
-    onModel:{
-        type:String,
-        required:true,
-        enum:['Post','Comment']
-    }
-
+    //Include the comment present in that post
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Comment'
+        }
+    ],
+    likes:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Like'
+        }
+    ]
 },{
     timestamps:true
 });
 
-const Like = mongoose.model('Like',likeSchema);
-module.exports = Like;
+const Post = mongoose.model("Post",postSchema);
+
+module.exports = Post;
